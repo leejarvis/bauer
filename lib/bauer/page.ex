5463d99503943a8ef7c0d724bbb177741ff5f785
Lee Jarvis <leejarvis@fastmail.com>
2016-07-20 15:31:35 +0100

    Add exdoc as a dependency

--- lib/bauer/page.ex
+++ lib/bauer/page.ex
@@ -1,10 +1,22 @@
 defmodule Bauer.Page do
+  @moduledoc """
+  This module provides convenience functions for extracting information
+  from a HTML page.
+
+  ## Examples
+
+      iex> %Bauer.Page{body: html} |> Bauer.Page.title() #=> "Hello, world!"
+  """
+
   defstruct [:body]
 
   alias Bauer.Page
 
   import Bauer.Finders
 
+  @doc """
+  Returns the HTML page title
+  """
   def title(%Page{body: body}) do
     body
     |> find("title")
--- mix.exs
+++ mix.exs
@@ -4,10 +4,15 @@ defmodule Bauer.Mixfile do
   def project do
     [app: :bauer,
      version: "0.1.0",
+     name: "Bauer",
+     source_url: "https://github.com/leejarvis/bauer",
+     homepage_url: "https://github.com/leejarvis/bauer",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     docs: docs()
+   ]
   end
 
   # Configuration for the OTP application
@@ -27,6 +32,13 @@ defmodule Bauer.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:floki, "~> 0.9.0"}]
+    [
+      {:floki, "~> 0.9.0"},
+      {:ex_doc, "~> 0.12", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [extras: ["README.md"]]
   end
 end
